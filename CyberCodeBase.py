3bf5497690488259b8dd8f9d095829df46bb2633
Kiana Lashgari <kiana_lashgari@ajgre.com>
2022-01-27 10:46:06 -0500

    Basic summary done

--- CyberCodeBase.py
+++ CyberCodeBase.py
@@ -2,8 +2,30 @@
 
 # This tool depends on the output of the Cyber Data Prep Tool. Specifically the Prism-Re Input Columns Template
 
-#Step 1 - Import the dataset into the Python environment. Change the file path below!
+# Step 1 - Import the dataset that includes Public Sector and/or Private Households into the Python environment. Change the file path below!
 
+from operator import index
 import pandas as pd
-data = pd.read_excel("T:\AFM\Cyber\Cyber Peer Portfolio\Peer Portfolio\Peer Portfolio 2021\Cyber-Data-Prep-Tool 20210908\Peer Portfolio 2021 (Total) - PRISM-Re Input Columns.xlsx")
-print(data)
+
+# CHANGE FILE PATH:
+data = pd.read_excel("C:\Kiana\Python testing.xlsx")
+
+# Exposure summary:
+total_prem = (data['Policy Premium at Participation'].sum())
+gross_prem = round(total_prem/1e6,2)
+ID_count = (data['Insured ID'].count())
+avg_lim = round((data['Breach 1st party\nCoverage Limit\nat 100%\n($)'].mean())/1e6,2)
+avg_att = round((data['Breach 1st party\nCoverage\nAttachment / Deductible\n($)'].mean())/1e6,2)
+avg_rev = round((data['Annual Revenue\n($M)'].mean())/1e6,2)
+
+exp_table = {'GWP ($M)': [gross_prem], 'Count': [ID_count], 'Average Limit($M)': [avg_lim], 'Average Attachment($M)': [avg_att], 'Average Revenue($M)':[avg_rev]}
+exposure_summary = pd.DataFrame(exp_table, columns= ['GWP ($M)', 'Count', 'Average Limit($M)', 'Average Attachment($M)', 'Average Revenue($M)'])
+
+# Sector summary:
+sectors = set(data['PRISM-Re Industry (breach)'])
+sector_summary = pd.DataFrame(data.groupby('PRISM-Re Industry (breach)')['Policy Premium at Participation'].sum()/total_prem)
+
+# CHANGE FILE PATH:
+with pd.ExcelWriter('C:\Kiana\Python output.xlsx', engine='xlsxwriter') as writer:
+    exposure_summary.to_excel(writer, sheet_name='Summary', index=False)
+    sector_summary.to_excel(writer, sheet_name='Summary', index=True, startrow=4)
