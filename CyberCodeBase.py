bf25f24deda5f20e8a67be20707a99790fbd8512
Kiana Lashgari <kiana_lashgari@ajgre.com>
2022-02-11 16:56:02 -0500

    Summary is completed

--- CyberCodeBase.py
+++ CyberCodeBase.py
@@ -2,10 +2,11 @@
 
 # This tool depends on the output of the Cyber Data Prep Tool. Specifically the Prism-Re Input Columns Template
 
-# Step 1 - Import the dataset that includes Public Sector and/or Private Households into the Python environment. Change the file path below!
+# MAKE SURE TO Import the dataset that includes Public Sector with revised Private Households sectors into the Python environment. Change the file path below!
 
 from operator import index
 import pandas as pd
+import numpy as np
 
 # CHANGE FILE PATH:
 data = pd.read_excel("C:\Kiana\Python testing.xlsx")
@@ -17,15 +18,23 @@ ID_count = (data['Insured ID'].count())
 avg_lim = round((data['Breach 1st party\nCoverage Limit\nat 100%\n($)'].mean())/1e6,2)
 avg_att = round((data['Breach 1st party\nCoverage\nAttachment / Deductible\n($)'].mean())/1e6,2)
 avg_rev = round((data['Annual Revenue\n($M)'].mean())/1e6,2)
+avg_security = round((data['Cybersecurity Level (1 = Above Average, 2 = Average, 3 = Below Average)'].mean()),2)
 
-exp_table = {'GWP ($M)': [gross_prem], 'Count': [ID_count], 'Average Limit($M)': [avg_lim], 'Average Attachment($M)': [avg_att], 'Average Revenue($M)':[avg_rev]}
-exposure_summary = pd.DataFrame(exp_table, columns= ['GWP ($M)', 'Count', 'Average Limit($M)', 'Average Attachment($M)', 'Average Revenue($M)'])
+exp_table = {'GWP ($M)': [gross_prem], 'Count': [ID_count], 'Average Limit ($M)': [avg_lim], 'Average Attachment ($M)': [avg_att], 'Average Revenue ($M)':[avg_rev], 'Average Cyber Security Level':[avg_security]}
+exposure_summary = pd.DataFrame(exp_table, columns= ['GWP ($M)', 'Count', 'Average Limit ($M)', 'Average Attachment ($M)', 'Average Revenue ($M)', 'Average Cyber Security Level'])
 
 # Sector summary:
 sectors = set(data['PRISM-Re Industry (breach)'])
 sector_summary = pd.DataFrame(data.groupby('PRISM-Re Industry (breach)')['Policy Premium at Participation'].sum()/total_prem)
 
+# Revenue summary:
+bins = [-1, 9.999999, 49.999999, 99.999999, 249.999999, 499.999999, 999.999999, 1999.999999, np.inf]
+labels = ['Less than $10M', '$10M - $50M', '$50M - $100M', '$100M - $250M', '$250M - $500M', '$500M - $1B', '$1B - $2B', 'Greater than $2B']
+rev_band = data.groupby(pd.cut(data['Annual Revenue\n($M)'], bins = bins , labels = labels))
+rev_band_sum = round(rev_band['Annual Revenue\n($M)'].sum(),2)
+
 # CHANGE FILE PATH:
 with pd.ExcelWriter('C:\Kiana\Python output.xlsx', engine='xlsxwriter') as writer:
     exposure_summary.to_excel(writer, sheet_name='Summary', index=False)
     sector_summary.to_excel(writer, sheet_name='Summary', index=True, startrow=4)
+    rev_band_sum.to_excel(writer, sheet_name='Summary', index=True, startrow=21)
